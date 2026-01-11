5e21f9b1738f8bbbd6ed50d9bfdb90f7d266e8e1
Gal Tibet <galtibet@gmail.com>
2026-01-11 18:23:58 +0200

    security: set search_path on handle_new_user_signup function

--- supabase/migrations/004_handle_new_user_signup.sql
+++ supabase/migrations/004_handle_new_user_signup.sql
@@ -59,7 +59,7 @@ BEGIN
 
   RETURN NEW;
 END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
+$$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = public;
 
 -- Create trigger to run after user signup
 DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
